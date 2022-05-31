229ff1b57f16fa3e0fb8c0fe31a5b2e3e63a7cea
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-31 11:55:33 -0400

    Route to update a user

--- Build REST API with Node.js/routes/users.js
+++ Build REST API with Node.js/routes/users.js
@@ -11,22 +11,39 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
     const user = req.body;
+
     // Create a new user with Id
     const userWithId = {...user, id: uuidv4()};
     users.push(userWithId);
+
     res.send("New user was added to database");
 });
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
     const foundUser = users.find((user) => user.id === id);
+
     res.send(foundUser);
 });
 
 router.delete('/:id', (req, res) => {
     const { id } = req.params;
     users = users.filter((user) => user.id !== id)
-    res.send('User with id ${id} was deleted');
+
+    res.send(`User with id ${id} was deleted`);
+});
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+
+    const foundUser = users.find((user) => user.id === id);
+
+    if (firstName) foundUser.firstName = firstName;
+    if (lastName) foundUser.lasttName = lasttName;
+    if (age) foundUser.age = age;
+    
+    res.send(`User with the id ${id} has been updated`);
 });
 
 export default router;
