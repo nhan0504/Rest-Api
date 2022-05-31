fabd500c29a399513196cc23d7b9d71564ccb8fe
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-31 05:10:37 -0400

    Using routes from routes folder

--- Build REST API with Node.js/index.js
+++ Build REST API with Node.js/index.js
@@ -2,11 +2,16 @@ import express from 'express';
 // Take in incoming POST request body
 import bodyParser from 'body-parser';
 
+// Import the router from the folder routes and named it userRoutes
+import userRoutes from "./routes/users.js";
+
 // app: The whole application
 const app = express();
 const PORT = 5000;
 // Use json data in the whole application
 app.use(bodyParser.json());
+
+app.use('/users', userRoutes);
 
 app.get('/', (req, res) => {res.send('Hello from Homepage')});
 
