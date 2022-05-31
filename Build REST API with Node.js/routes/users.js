3bace6feb580ed47e727579af4ac52ea424c9eaa
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-31 05:36:27 -0400

    response with all users data

--- Build REST API with Node.js/routes/users.js
+++ Build REST API with Node.js/routes/users.js
@@ -2,8 +2,17 @@ import express from "express";
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "John",
+        lastName: "Doe",
+        age: 25
+    }
+]
+
 router.get('/', (req, res) => {
-    res.send("Hello");
+    console.log(users);
+    res.send(users);
 })
 
 export default router;
