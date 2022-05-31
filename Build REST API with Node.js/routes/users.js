6bd7723203d171edef88627d3b3e5c2aeea18447
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-31 11:32:02 -0400

    Route to delete a user

--- Build REST API with Node.js/routes/users.js
+++ Build REST API with Node.js/routes/users.js
@@ -3,12 +3,11 @@ import {v4 as uuidv4} from 'uuid';
 
 const router = express.Router();
 
-const users = [];
+let users = [];
 
 router.get('/', (req, res) => {
-    console.log(users);
     res.send(users);
-})
+});
 
 router.post('/', (req, res) => {
     const user = req.body;
@@ -22,6 +21,12 @@ router.get('/:id', (req, res) => {
     const { id } = req.params;
     const foundUser = users.find((user) => user.id === id);
     res.send(foundUser);
-})
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    users = users.filter((user) => user.id !== id)
+    res.send('User with id ${id} was deleted');
+});
 
 export default router;
