f131ae8bd1e173e986caa6f84154fbaa84afe329
Nhan0504 <ttn0504ltt@gmail.com>
2022-05-31 23:25:44 -0400

    Mkae routes look clearer

--- Build REST API with Node.js/routes/users.js
+++ Build REST API with Node.js/routes/users.js
@@ -1,49 +1,17 @@
 import express from "express";
-import {v4 as uuidv4} from 'uuid';
+import { getUser, createUser, getUserByID, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [];
 
-router.get('/', (req, res) => {
-    res.send(users);
-});
+router.get('/', getUser);
 
-router.post('/', (req, res) => {
-    const user = req.body;
+router.post('/', createUser);
 
-    // Create a new user with Id
-    const userWithId = {...user, id: uuidv4()};
-    users.push(userWithId);
+router.get('/:id', getUserByID);
 
-    res.send("New user was added to database");
-});
+router.delete('/:id', deleteUser);
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    const foundUser = users.find((user) => user.id === id);
-
-    res.send(foundUser);
-});
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    users = users.filter((user) => user.id !== id)
-
-    res.send(`User with id ${id} was deleted`);
-});
-
-router.patch('/:id', (req, res) => {
-    const { id } = req.params;
-    const { firstName, lastName, age } = req.body;
-
-    const foundUser = users.find((user) => user.id === id);
-
-    if (firstName) foundUser.firstName = firstName;
-    if (lastName) foundUser.lasttName = lasttName;
-    if (age) foundUser.age = age;
-    
-    res.send(`User with the id ${id} has been updated`);
-});
+router.patch('/:id', updateUser);
 
 export default router;
